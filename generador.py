13019a99e9cdf0efa50bb95df7f2caa0fb6bab7f
Santiago del Palacio <santiagodp1990@gmail.com>
2020-06-08 16:52:51 -0300

    Modificado el generador para que haya un tipo de datos diferente (gaussiano o sinusoidal) para los proyectos 1 y 2. Los archivos de datos resultantes fueron actualizados.

--- generador.py
+++ generador.py
@@ -1,5 +1,5 @@
 #Script para generar archivos de datos
-# de tiempo e fuente
+# de tiempo e intensidad para cada fuente
 
 import numpy as np
 import sys
@@ -11,7 +11,15 @@ num = sys.argv[2]
 t = np.linspace(0,100, 200)
 
 #datos
-datos = np.random.random(t.shape)
-datos += 1000*stats.norm.pdf(t,np.random.randint(30,70),5 )
+if proy == '1':
+    #gaussiana con media en un valor random entre 30 y 70, y varianza = 5
+    datos = 1000*stats.norm.pdf(t,np.random.randint(30,70),5 )
+
+elif proy == '2':
+    #sinusoide de amplitud A y periodo P + errores aleatorios de amplitud 1
+    A, P = 10, 50
+    datos = A*np.sin(t*(2*np.pi/P))
+    datos += np.random.random(t.shape)
+
 
 np.savetxt('proyecto{}/archivo{}.dat'.format(proy,num), np.transpose([t,datos]))
