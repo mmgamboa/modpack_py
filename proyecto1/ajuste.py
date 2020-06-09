36f7ea0586b2d851534e1f2020779a8f5b1d7b2d
mmgamboa <martingamboa2@gmail.com>
2020-06-08 21:15:18 -0300

    m1 o m2 funcionando

--- proyecto1/ajuste.py
+++ proyecto1/ajuste.py
@@ -6,12 +6,16 @@ import pylab as plt
 #Importamos modulo especifico de nuestro proyecto
 from model import m1,m2
 
-def ajusto(m,x,y):
+def ajusto(x,y,m):
 	"""Ajuste de datos utilizando modelo m"""
-	popt, _ = curve_fit(m, x, y)
+	if m=='m1':
+		popt, _ = curve_fit(m1, x, y)
+	elif m == 'm2':
+		popt, _ = curve_fit(m2, x, y)
+
 	return popt
 
-def grafica(x,y, modelo, pars):
+def grafica(m,x,y, pars):
 	"""Esta funcion realiza un grafico y el grafico de una funcion ajustada con los mismos datos
 	x: x-data de la forma de array de numpy
 	y: y-data de la forma de array de numpy
@@ -22,6 +26,9 @@ def grafica(x,y, modelo, pars):
 	plt.xlabel('t[seg]')
 	plt.ylabel('I[un. arb.]')
 	plt.plot(x, y, 'ko',alpha=0.5, label = 'Datos')
-	plt.plot(x, modelo(x, *pars), 'r-', label='Ajuste')
+	if m == 'm1':
+		plt.plot(x, m1(x, *pars), 'r-', label='Ajuste')
+	elif m == 'm2':
+		plt.plot(x, m2(x, *pars), 'r-', label='Ajuste')
 	plt.legend(loc='best')
 	plt.show()
